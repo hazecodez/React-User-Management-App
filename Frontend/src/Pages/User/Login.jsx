0a262a4867f15b02acb28d69668ad16a57fb2189
hazecodez <muhammedhashir298@gmail.com>
2024-01-11 14:44:15 +0530

    Signup validation done.

--- Frontend/src/Pages/User/Login.jsx
+++ Frontend/src/Pages/User/Login.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Card, Input, Button, Typography } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate()
   return (
-    <div>
-      Login
+    <div className="w-full flex justify-center items-center h-screen">
+      <Card color="transparent" shadow={false}>
+        <Typography variant="h4" color="blue-gray">
+          Log In
+        </Typography>
+        <Typography color="gray" className="mt-1 font-normal">
+          Nice to meet you! Enter your email and password to login.
+        </Typography>
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+          <div className="mb-1 flex flex-col gap-6">
+            
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Email
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Password
+            </Typography>
+            <Input
+              type="password"
+              size="lg"
+              placeholder="********"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+          </div>
+
+          <Button className="mt-6" fullWidth>
+            Log In
+          </Button>
+          <Typography color="gray" className="mt-4 text-center font-normal">
+            Don't have an account?{" "}
+            <a onClick={()=> navigate('/signup')} className="font-medium text-gray-900">
+              Sign Up
+            </a>
+          </Typography>
+        </form>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- Frontend/src/Pages/User/Signup.jsx
+++ Frontend/src/Pages/User/Signup.jsx
@@ -1,11 +1,180 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Card, Input, Button, Typography } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
+
+import "react-toastify/dist/ReactToastify.css";
 
 function Signup() {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [nameAlert, setNameAlert] = useState("");
+  const [emailAlert, setEmailAlert] = useState("");
+  const [numberAlert, setNumAlert] = useState("");
+  const [passAlert, setPassAlert] = useState("");
+
+  //Validation and data save to store (Signup handling)
+  const signupHandle = async (e) => {
+    e.preventDefault();
+    try {
+      if (name.trim() === "" || nameAlert) {
+        setNameAlert("Must fillout the field.");
+      } else if (email.trim() === "" || emailAlert) {
+        setEmailAlert("Must fillout the field.");
+      } else if (number.trim() === "" || numberAlert) {
+        setNumAlert("Must fillout the field.");
+      } else if (password.trim() === "" || passAlert) {
+        setPassAlert("Must fillout the field.");
+      } else {
+        console.log("settt");
+      }
+    } catch (error) {}
+  };
+
   return (
-    <div>
-      signup
+    <div className="w-full flex justify-center items-center h-screen">
+      <Card color="transparent" shadow={false}>
+        <Typography variant="h4" color="blue-gray">
+          Sign Up
+        </Typography>
+        <Typography color="gray" className="mt-1 font-normal">
+          Nice to meet you! Enter your details to register.
+        </Typography>
+        <form
+          onSubmit={signupHandle}
+          className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+        >
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Name
+            </Typography>
+            <Input
+              size="lg"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+                if (e.target.value.length < 4 || e.target.value.trim() === "") {
+                  setNameAlert("Username must be 4 letters.");
+                } else {
+                  setNameAlert("");
+                }
+              }}
+              placeholder="John Doe"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            {nameAlert && (
+              <p style={{ color: "red" }}>
+                <i className="fa-solid fa-triangle-exclamation" />
+                &nbsp;&nbsp;&nbsp;{nameAlert}
+              </p>
+            )}
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Email
+            </Typography>
+            <Input
+              size="lg"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+
+                if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e.target.value)) {
+                  setEmailAlert("Please enter a valid email address.");
+                } else {
+                  setEmailAlert("");
+                }
+              }}
+              placeholder="name@mail.com"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            {emailAlert && (
+              <p style={{ color: "red" }}>
+                <i className="fa-solid fa-triangle-exclamation" />
+                &nbsp;&nbsp;&nbsp;{emailAlert}
+              </p>
+            )}
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Number
+            </Typography>
+            <Input
+              type="number"
+              value={number}
+              onChange={(e) => {
+                setNumber(e.target.value);
+                if (
+                  e.target.value.length !== 10 ||
+                  e.target.value === "0000000000"
+                ) {
+                  setNumAlert("Please enter a valid mobile number.");
+                } else {
+                  setNumAlert("");
+                }
+              }}
+              size="lg"
+              placeholder="******6543"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            {numberAlert && (
+              <p style={{ color: "red" }}>
+                <i className="fa-solid fa-triangle-exclamation" />
+                &nbsp;&nbsp;&nbsp;{numberAlert}
+              </p>
+            )}
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Password
+            </Typography>
+            <Input
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                if (e.target.value.length < 6) {
+                  setPassAlert("Password must be 6 characters or longer.");
+                } else {
+                  setPassAlert("");
+                }
+              }}
+              type="password"
+              size="lg"
+              placeholder="********"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            {passAlert && (
+              <p style={{ color: "red" }}>
+                <i className="fa-solid fa-triangle-exclamation" />
+                &nbsp;&nbsp;&nbsp;{passAlert}
+              </p>
+            )}
+          </div>
+
+          <Button className="mt-6" fullWidth type="submit">
+            sign up
+          </Button>
+          <Typography color="gray" className="mt-4 text-center font-normal">
+            Already have an account?{" "}
+            <a
+              onClick={() => navigate("/login")}
+              className="font-medium text-gray-900"
+            >
+              Log In
+            </a>
+          </Typography>
+        </form>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
